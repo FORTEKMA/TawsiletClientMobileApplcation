84bf8761a24883819bab3320aedf1b0ada8fbd1f
ghoudi01 <ghoudi31@gmail.com>
2025-07-29 14:49:44 +0100

    g

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
@@ -12,16 +12,18 @@ AppRegistry.registerComponent(appName, () => App);
 import VoipPushNotification from 'react-native-voip-push-notification';
 
 // Register VoIP push notification event listener
-VoipPushNotification.addEventListener('register', (token) => {
-  // --- send token to your apn server ----
-});
+if (Platform.OS === 'ios') {
+  VoipPushNotification.addEventListener('register', (token) => {
+    // --- send token to your apn server ----
+  });
 
-VoipPushNotification.addEventListener('notification', (notification) => {
-  // --- when user answer the call, you should navigate to the call screen and update callKeep status ----
-  // --- such as RNCallKeep.backToForeground() and RNCallKeep.endAllCalls() ----
-  VoipPushNotification.invokeApp();
-});
+  VoipPushNotification.addEventListener('notification', (notification) => {
+    // --- when user answer the call, you should navigate to the call screen and update callKeep status ----
+    // --- such as RNCallKeep.backToForeground() and RNCallKeep.endAllCalls() ----
+    VoipPushNotification.invokeApp();
+  });
 
-VoipPushNotification.registerVoipToken();
+  VoipPushNotification.registerVoipToken();
+}
 
 
